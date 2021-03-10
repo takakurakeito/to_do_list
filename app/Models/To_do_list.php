7319f3cd9f8a0d0ec1b3ed3b829882de031c7c53
takakurakeito <takakura.0820@gmail.com>
2021-03-10 10:31:00 +0900

    php_file

---
+++ app/Models/To_do_list.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class To_do_list extends Model
+{
+    use HasFactory;
+}
